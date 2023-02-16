4bbea1777ef9b90d351bc9df97cb97b3940e35c8
adarsh-pv <adarshharindran112@gmail.com>
2023-02-16 17:46:42 +0530

    crud with mysql and nodejs

--- controller/userController.js
+++ controller/userController.js
@@ -2,20 +2,17 @@ import db from '../db/connection.js'
 
 export const inserUser = (req,res)=>{
     db.query(`INSERT INTO blocks (id, name) VALUES (20, 'raja')`,(error,result,failed)=>{
-          console.log(error,"error")
-          console.log(result)
+        error ? console.log(error) : console.log(result)
 })
 }
 export const findAllUsers =  (req,res)=>{
     db.query(`SELECT * FROM blocks WHERE 1`,(error,result,failed)=>{
-     console.log(error)
-     console.log(result,"response")//here get all users array
+        error ? console.log(error) : console.log(result)//here get all users array
     })
 } 
 export const updateUser = (req,res)=>{
     db.query(`UPDATE blocks SET name='abhi' WHERE id=22`,(error,result,failed)=>{
-        console.log(error)
-        console.log(result,"result")
+        error ? console.log(error) : console.log(result)
     })
 }
 export const deleteUser = (req,res)=>{
